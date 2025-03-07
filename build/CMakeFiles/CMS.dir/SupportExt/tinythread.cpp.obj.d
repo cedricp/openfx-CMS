6be1e33d07c0ac77db0a49b951a3e2733c55de21
cedric paillé <cedricpaille@gmail.com>
2025-03-07 18:05:34 +0100

    Remove build directory

--- build/CMakeFiles/CMS.dir/SupportExt/tinythread.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/CMS.dir/SupportExt/tinythread.cpp.obj: \
-  D:/Development/sources/openfx-CMS/SupportExt/tinythread.cpp
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Development/sources/openfx-CMS
-# Build directory: D:/Development/sources/openfx-CMS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
